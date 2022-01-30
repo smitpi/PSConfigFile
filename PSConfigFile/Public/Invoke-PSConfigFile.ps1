4c4ee829ce92d0bdd174477b99d44b069c895452
smitpi <pierre.smit@outlook.com>
2022-01-30 12:53:46 +0200

    f

--- PSConfigFile/Public/Invoke-PSConfigFile.ps1
+++ PSConfigFile/Public/Invoke-PSConfigFile.ps1
@@ -61,6 +61,9 @@ Executes the config from the json file.
 .PARAMETER ConfigFile
 Path to the the config file ($PSConfigfile is a default variable created with the config file)
 
+.PARAMETER DisplayOutput
+Do not show the output on the console.
+
 .EXAMPLE
 Invoke-PSConfigFile -ConfigFile C:\Temp\jdh\PSCustomConfig.json
 
@@ -70,77 +73,95 @@ Function Invoke-PSConfigFile {
     param (
         [parameter(Mandatory)]
         [ValidateScript( { (Test-Path $_) -and ((Get-Item $_).Extension -eq '.json') })]
-        [System.IO.FileInfo]$ConfigFile
+        [System.IO.FileInfo]$ConfigFile,
+        [switch]$DisplayOutput = $false
     )
+    $output = New-Item (Join-Path $env:TEMP -ChildPath "$(Get-Random).txt")
+    & {
     try {
-        $confile = Get-Item $ConfigFile -ErrorAction SilentlyContinue
-        $logfile = Join-Path $confile.DirectoryName -ChildPath PSCustomConfigLog.log
-        if ((Test-Path $logfile) -eq $false) { New-Item -Path $logfile -ItemType File -Force | Out-Null }
+            $confile = Get-Item $ConfigFile -ErrorAction SilentlyContinue
+            $logfile = Join-Path $confile.DirectoryName -ChildPath PSCustomConfigLog.log
+            if ((Test-Path $logfile) -eq $false) { New-Item -Path $logfile -ItemType File -Force | Out-Null }
 
-        Write-Color 'PSConfigFile Execution Start' -ShowTime -Color DarkCyan -LinesBefore 4 -LogFile $logfile
-        Write-Color '#######################################################' -ShowTime -Color Green -LogFile $logfile
-
-        $JSONParameter = (Get-Content $confile.FullName | Where-Object { $_ -notlike "*`"Default`"*" }) | ConvertFrom-Json | Tee-Object -FilePath $logfile -Append
-        if ($null -eq $JSONParameter) { Write-Error 'Valid Parameters file not found'; break }
-        Write-Color 'Using PSCustomConfig file: ', $($confile.fullname) -ShowTime -Color DarkCyan, DarkYellow -LogFile $logfile
-
-        # User Data
-        Write-Color 'Details of Config File:' -ShowTime -Color DarkCyan -LinesBefore 1
-        $JSONParameter.Userdata.PSObject.Properties | ForEach-Object { Write-Color $_.name, ':', $_.value -Color Yellow, DarkCyan, Green -ShowTime -StartTab 4 }
-
-        # Set Location
-        if ([bool]$JSONParameter.SetLocation.WorkerDir -like $true) {
-            Write-Color 'Setting Folder Location: ', $($JSONParameter.SetLocation.WorkerDir) -ShowTime -Color DarkCyan, DarkYellow -LinesBefore 1 -LogFile $logfile
-            Set-Location $JSONParameter.SetLocation.WorkerDir -ErrorAction SilentlyContinue
-        }
-
-        #Set Variables
-        Write-Color 'Setting Default Variables:' -ShowTime -Color DarkCyan -LinesBefore 1 -LogFile $logfile
-        $JSONParameter.SetVariable.PSObject.Properties | Sort-Object -Property name | ForEach-Object {
-            Write-Color $_.name, ':', $_.value -Color Yellow, DarkCyan, Green -ShowTime -LogFile $logfile
-            New-Variable -Name $_.name -Value $_.value -Force -Scope global
-        }
-        Write-Color 'PSConfigFilePath', ':', ($confile.Directory).FullName -Color Yellow, DarkCyan, Green -ShowTime -LogFile $logfile
-        New-Variable -Name 'PSConfigFilePath' -Value ($confile.Directory).FullName -Scope global -Force
-        Write-Color 'PSConfigFile', ':', $confile.FullName -Color Yellow, DarkCyan, Green -ShowTime -LogFile $logfile
-        New-Variable -Name 'PSConfigFile' -Value $confile.FullName -Scope global -Force
-
-        # Set PsDrives
-        Write-Color 'Creating PSDrives:' -ShowTime -Color DarkCyan -LinesBefore 1 -LogFile $logfile
-        $JSONParameter.PSDrive.PSObject.Properties | ForEach-Object { Write-Color $_.name, ':', $_.value.root -Color Yellow, DarkCyan, Green -ShowTime -LogFile $logfile -NoNewLine
-            if (-not(Get-PSDrive -Name $_.name -ErrorAction SilentlyContinue)) {
-                New-PSDrive -Name $_.name -PSProvider FileSystem -Root $_.value.root -Scope Global | Out-Null
-                Write-Color ' - Mapped' -Color Yellow -LogFile $logfile
+            Write-Output "[$((Get-Date -Format HH:mm:ss).ToString())] PSConfigFile Execution Start"
+            Write-Output "[$((Get-Date -Format HH:mm:ss).ToString())] #######################################################"
+            Write-Output " "
+            
+            $JSONParameter = (Get-Content $confile.FullName | Where-Object { $_ -notlike "*`"Default`"*" }) | ConvertFrom-Json
+            if ($null -eq $JSONParameter) { Write-Error 'Valid Parameters file not found'; break }
+            Write-Output "[$((Get-Date -Format HH:mm:ss).ToString())] Using PSCustomConfig file: $($confile.fullname)"
+            
+            # User Data
+            Write-Output "  "
+            Write-Output "[$((Get-Date -Format HH:mm:ss).ToString())] Details of Config File:"
+            $JSONParameter.Userdata.PSObject.Properties | ForEach-Object { 
+                  Write-Output "[$((Get-Date -Format HH:mm:ss).ToString())] `t`t$($_.name) : $($_.value)"  
             }
-            else { Write-Color ' - Already exists' -Color Yellow -LogFile $logfile }
-        }
 
-        # Set Alias
-        Write-Color 'Creating Custom Aliases: ' -ShowTime -Color DarkCyan -LinesBefore 1 -LogFile $logfile
-        $JSONParameter.PSAlias.PSObject.Properties | Select-Object name, value | Sort-Object -Property Name | ForEach-Object {
-            $tmp = $null
-            Write-Color $_.name, ':', $_.value -Color Yellow, DarkCyan, Green -ShowTime -LogFile $logfile
-            $command = "function global:$($_.name) {$($_.value)}"
-            $tmp = [scriptblock]::Create($command)
-            $tmp.invoke() | Tee-Object -FilePath $logfile -Append
-        }
+            # Set Location
+            if ([bool]$JSONParameter.SetLocation.WorkerDir -like $true) {
+                Write-Output "  "
+                Write-Output "[$((Get-Date -Format HH:mm:ss).ToString())] Setting Folder Location: $($JSONParameter.SetLocation.WorkerDir)"
+                Set-Location $JSONParameter.SetLocation.WorkerDir -ErrorAction SilentlyContinue
+            }
 
-        # Execute Commands
-        Write-Color 'Executing Custom Commands: ' -ShowTime -Color DarkCyan -LinesBefore 1 -LogFile $logfile
-        $JSONParameter.execute.PSObject.Properties | Select-Object name, value | Sort-Object -Property Name | ForEach-Object {
-            $tmp = $null
-            Write-Color $_.name, ':', $_.value -Color Yellow, DarkCyan, Green -ShowTime -LogFile $logfile -LinesBefore 1
-            Write-Color 'ScriptBlock Output:' -Color Yellow -ShowTime -LogFile $logfile
-            $tmp = [scriptblock]::Create($_.value)
-            $tmp.invoke() | Tee-Object -FilePath $logfile -Append
-        }
+            #Set Variables
+            Write-Output "[$((Get-Date -Format HH:mm:ss).ToString())] Setting Default Variables:"
+            $JSONParameter.SetVariable.PSObject.Properties | Sort-Object -Property name | ForEach-Object {
+                Write-Output "  "
+                Write-Output "[$((Get-Date -Format HH:mm:ss).ToString())] $($_.name) : $($_.value)"
+                New-Variable -Name $_.name -Value $_.value -Force -Scope global
+            }
+            Write-Output "[$((Get-Date -Format HH:mm:ss).ToString())] PSConfigFilePath : ($confile.Directory).FullName"
+            New-Variable -Name 'PSConfigFilePath' -Value ($confile.Directory).FullName -Scope global -Force
+            Write-Output "[$((Get-Date -Format HH:mm:ss).ToString())] PSConfigFile : $($confile.FullName)"
+            New-Variable -Name 'PSConfigFile' -Value $confile.FullName -Scope global -Force
 
-        Write-Color '#######################################################' -ShowTime -Color Green -LinesBefore 1 -LogFile $logfile
-        Write-Color 'PSConfigFile Execution End' -ShowTime -Color DarkCyan -LogFile $logfile
+            # Set PsDrives
+            Write-Output "  "
+            Write-Output "[$((Get-Date -Format HH:mm:ss).ToString())] Creating PSDrives:"
+            $JSONParameter.PSDrive.PSObject.Properties | ForEach-Object { Write-Output "[$((Get-Date -Format HH:mm:ss).ToString())] $($_.name) : $($_.value.root)"
+                if (-not(Get-PSDrive -Name $_.name -ErrorAction SilentlyContinue)) {
+                    New-PSDrive -Name $_.name -PSProvider FileSystem -Root $_.value.root -Scope Global | Out-Null
+                }
+                else { Write-Warning '`nWarning: PSDrive - Already exists'}
+            }
+
+            # Set Alias
+           Write-Output "[$((Get-Date -Format HH:mm:ss).ToString())] Creating Custom Aliases: "
+            $JSONParameter.PSAlias.PSObject.Properties | Select-Object name, value | Sort-Object -Property Name | ForEach-Object {
+                $tmp = $null
+                Write-Output "[$((Get-Date -Format HH:mm:ss).ToString())] $($_.name) : $($_.value)"
+                $command = "function global:$($_.name) {$($_.value)}"
+                $tmp = [scriptblock]::Create($command)
+                $tmp.invoke() | Tee-Object -FilePath $logfile -Append
+            }
+
+            # Execute Commands
+            Write-Output "[$((Get-Date -Format HH:mm:ss).ToString())] Executing Custom Commands: "
+            $JSONParameter.execute.PSObject.Properties | Select-Object name, value | Sort-Object -Property Name | ForEach-Object {
+                $tmp = $null
+                Write-Output "[$((Get-Date -Format HH:mm:ss).ToString())] $($_.name) : $($_.value)"
+                Write-Output "[$((Get-Date -Format HH:mm:ss).ToString())] ScriptBlock Output:"
+                $tmp = [scriptblock]::Create($_.value)
+                $tmp.invoke() | Tee-Object -FilePath $logfile -Append
+            }
+
+           Write-Output "[$((Get-Date -Format HH:mm:ss).ToString())] #######################################################"
+           Write-Output "[$((Get-Date -Format HH:mm:ss).ToString())] PSConfigFile Execution End"
     }
     catch {
-        Write-Output 'An Error...' | Tee-Object -FilePath $logfile -Append
-        $_.Exception | Tee-Object -FilePath $logfile -Append
-        $_.Exception.message | Tee-Object -FilePath $logfile -Append
+        Write-Error "An Error...:`n $_.Exception `n $_.Exception.message"
     }
+    } *> $output.FullName
+
+    $Feedback = Get-Content $output.FullName
+    $warning = Select-String -Path $output.FullName -Pattern 'Warning'
+    $configErrors = Select-String -Path $output.FullName -Pattern 'Error'
+
+    if ($DisplayOutput){ $Feedback}
+    else {Write-Output "Invoke-PSConfigFile Completed: $($warning.count) Warnings; $($configErrors.count) Errors"}
+
+    
+
 } #end Function
